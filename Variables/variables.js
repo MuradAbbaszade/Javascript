4971bec469e313c3a10198f028136e2e04fb8229
abbaszade <muradabbaszade6@gmail.com>
2022-11-08 20:44:05 +0400

    Add data types task

--- Variables/variables.js
+++ Variables/variables.js
@@ -1,4 +1,9 @@
+//Variables
 let country = 'Azerbaijan';
 let continent = 'Europe';
 let population = 10000000;
 console.log(country + ' ' + continent + ' ' + population)
+//Data types
+let isIsland = false;
+let language;
+console.log(isIsland + ' ' + country + ' ' + continent + ' ' + population + ' ' + language)
