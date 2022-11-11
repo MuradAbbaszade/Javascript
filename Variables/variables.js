aabcc57c749afe46c5bc94cd40aa6578314672c0
abbaszade <muradabbaszade6@gmail.com>
2022-11-11 19:45:43 +0400

    Add codding challange

--- Variables/variables.js
+++ Variables/variables.js
@@ -7,3 +7,12 @@ console.log(country + ' ' + continent + ' ' + population)
 let isIsland = false;
 let language;
 console.log(isIsland + ' ' + country + ' ' + continent + ' ' + population + ' ' + language)
+//Coding challenge
+let markMass = 78;
+let johnMass = 92;
+let johnHeight = 1.95;
+let markHeight = 1.69;
+let markBMI = markMass / (markHeight ** 2);
+let johnBMI = johnMass / (johnHeight ** 2);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI); 
