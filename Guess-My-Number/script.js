88747ba72d748e78bd725078ffb57f969f4f4fc3
abbaszade <muradabbaszade6@gmail.com>
2022-11-19 22:29:37 +0400

    Update guess game

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -2,13 +2,18 @@ let number = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 let win = false;
 let highscore = 0;
+let message = "";
+const displayMessage = function(message) {
+  document.querySelector(".message").textContent = message;
+};
 console.log(number);
 document.querySelector(".check").addEventListener("click", function() {
   if (!win) {
     const guess = Number(document.querySelector(".guess").value);
     if (guess && guess >= 1 && guess <= 20) {
       if (guess == number) {
-        document.querySelector(".message").textContent = "Congrulations!";
+        win = true;
+        displayMessage("Congrulations!");
         if (highscore < score) {
           highscore = score;
           document.querySelector(".highscore").textContent = score;
@@ -16,20 +21,19 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".number").textContent = number;
         document.querySelector("body").style.backgroundColor = "#60b347";
         return;
-      } else if (guess < number) {
-        document.querySelector(".message").textContent = "Too Low";
-      } else {
-        document.querySelector(".message").textContent = "Too High";
-      }
-      if (score > 0) {
-        score--;
-        document.querySelector(".score").textContent = score;
-      } else {
-        document.querySelector(".message").textContent = "Losing...";
+      } else if (guess != number) {
+        if (score > 0) {
+          score--;
+          message = guess > number ? "Too high!" : "To low!";
+          document.querySelector(".score").textContent = score;
+        } else {
+          message = "Losing...";
+        }
       }
     } else {
-      document.querySelector(".message").textContent = "Invalid number...";
+      message = "Invalid number...";
     }
+    displayMessage(message);
   }
 });
 
