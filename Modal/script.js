2382369e294472f96d6eb4cdf3ad7b0b493d45a9
abbaszade <muradabbaszade6@gmail.com>
2022-11-20 11:50:48 +0400

    Initial commit

--- Modal/script.js
+++ Modal/script.js
@@ -4,15 +4,24 @@ const openModalButtons = document.querySelectorAll(".show-modal");
 const closeModalButton = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
-for (let i = 0; i < openModalButtons.length; i++) {
-  openModalButtons[i].addEventListener("click", function() {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
+const openModal = function() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
 const closeModal = function() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+
+for (let i = 0; i < openModalButtons.length; i++) {
+  openModalButtons[i].addEventListener("click", openModal);
+}
 closeModalButton.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function(e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
